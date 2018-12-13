a3aa25617c0ed09a612c12788c963aa1f30c1cee
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-12 18:17:50 -0800

    Fixed syntactical error in Node inquirer prompt; prompt running properly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,31 @@
+const nodeInput = process.argv[2];
 const inquirer = require("inquirer");
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-inquirer.prompt([
-    {
-        type: "list",
-        message: "What product ID would you like to purchase?",
-        choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        name: "productChoice"
-    },
-    {
-        type: "list",
-        message: "How many units of this product would you like to buy?"
-        choices: [1,2,3,4,5,6,7,8,9,10, "Enter Your Own Quantity"],
-        name: "productQuantity"
-    }
-]).then(function (answer) {
-    console.log(answer.productQuantity);
-    console.log(answer.productChoice);
-});
+
+
+if (nodeInput === "runPrompt") {
+    runPrompt();
+}
+
+function runPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What product ID would you like to purchase?",
+                choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+                name: "productChoice"
+            },
+            {
+                type: "list",
+                message: "How many units of this product would you like to buy?",
+                choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Enter your quantity"],
+                name: "productQuantity"
+            }
+        ])
+        .then(function (answer) {
+            console.log(`You Chose: ${answer.productChoice}`);
+            console.log(`You Chose: ${answer.productQuantity}`);
+        });
+}
