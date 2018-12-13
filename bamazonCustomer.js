38f3886082b4173e526ee05242ddad6c8c2fcfac
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-12 17:56:10 -0800

    Two questions added to inquirer prompt for testing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,19 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 inquirer.prompt([
-
+    {
+        type: "list",
+        message: "What product ID would you like to purchase?",
+        choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        name: "productChoice"
+    },
+    {
+        type: "list",
+        message: "How many units of this product would you like to buy?"
+        choices: [1,2,3,4,5,6,7,8,9,10, "Enter Your Own Quantity"],
+        name: "productQuantity"
+    }
 ]).then(function (answer) {
-
+    console.log(answer.productQuantity);
+    console.log(answer.productChoice);
 });
