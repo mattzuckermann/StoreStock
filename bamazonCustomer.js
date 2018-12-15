bd7eeee77d3282eef2e619eb24f73a95f101cfe5
mczuckermann <matt.zuckermann@my.wheaton.edu>
2018-12-15 00:10:32 -0800

    Cleaned up console output formatting and created more exits

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-console.log(`=================================
-Your action is being processed...
+console.log(`
+=================================
+Attempting to connect to database
 =================================
 `);
 
@@ -16,7 +17,7 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    else console.log(`Your connection to port #${connection.config.port} was successful!`);
+    else console.log(`Your connection to port #${connection.config.port} was successful!\n`);
     runPrompt();
 });
 
@@ -26,12 +27,12 @@ function runPrompt() {
             {
                 type: "list",
                 message: "What product ID would you like to purchase?",
-                choices: [`I DON'T WANT TO PURCHASE ANYTHING`, "1: Car", "2: Vase", "3: Wrench", "4: Motorcycle", "5: Chair", "6: Screwdriver", "7: Math Textbook", "8: Spiral Bound Notebook", "9: Permanent Markers", "10: Electric Guitar"],
+                choices: ["* - I DON'T WANT TO PURCHASE ANYTHING", "1 - Car", "2 - Vase", "3 - Wrench", "4 - Motorcycle", "5 - Chair", "6 - Screwdriver", "7 - Math Textbook", "8 - Spiral Bound Notebook", "9 - Permanent Markers", "10 - Electric Guitar"],
                 name: "productChoiceName"
             }
         ]).then(function (answer) {
             let productChoiceName = answer.productChoiceName;
-            if (answer.productChoiceName === "I DON'T WANT TO PURCHASE ANYTHING") {
+            if (answer.productChoiceName === "* - I DON'T WANT TO PURCHASE ANYTHING") {
                 process.exit();
             } else {
                 inquirer
@@ -45,10 +46,8 @@ function runPrompt() {
                     ])
                     .then(function (answer) {
                         try {
-                            console.log(productChoiceName);
-                            const arrayOfProductChoice = (productChoiceName).split(": ");
+                            const arrayOfProductChoice = (productChoiceName).split(" - ");
                             const idOfProductChoice = arrayOfProductChoice[0];
-                            console.log(idOfProductChoice);
                             connection.query(
                                 `SELECT * FROM products WHERE id = ${idOfProductChoice}`,
                                 function queryOne(err, res) {
@@ -56,10 +55,10 @@ function runPrompt() {
                                     const queryItemName = res[0].product_name;
                                     let queryItemQuantity = res[0].stock_quantity;
                                     const queryItemCost = res[0].price;
-                                    if (answer.productChoiceQuantity === 0 || answer.productChoiceQuantity === 1) {
-                                        console.log(`Processing purchase order for * ${answer.productChoiceQuantity} ${queryItemName} *`);
+                                    if (answer.productChoiceQuantity === 1) {
+                                        console.log(`\nProcessing purchase order for ${answer.productChoiceQuantity} ${queryItemName}`);
                                     } else {
-                                        console.log(`Processing purchase order for * ${answer.productChoiceQuantity} ${queryItemName}s *`);
+                                        console.log(`\nProcessing purchase order for ${answer.productChoiceQuantity} ${queryItemName}s`);
                                     }
 
                                     // return queryItemQuantity;
@@ -67,18 +66,34 @@ function runPrompt() {
                                     if (answer.productChoiceQuantity > queryItemQuantity) {
                                         const quantityDifference = (answer.productChoiceQuantity - queryItemQuantity);
                                         console.log(`
-Insufficient Quantity
----------------------
+===========================================
+Insufficient Quantity!
+
 You have ordered ${quantityDifference} too many.
-We only have ${queryItemQuantity} in stock.
-Please try again, ordering less than the total quantity in stock.
----------------------
+We have ${queryItemQuantity} left in stock.
+Be sure to order less than the total stock.
+===========================================
 `);
-                                        runPrompt();
+                                        inquirer
+                                            .prompt([
+                                                {
+                                                    type: "confirm",
+                                                    message: "Would you still like to make a purchase?",
+                                                    name: "choicePurchase"
+                                                }
+                                            ])
+                                            .then(function (answer) {
+                                                if (answer.choicePurchase === true) {
+                                                    runPrompt();
+                                                } else {
+                                                    console.log("");
+                                                    process.exit();
+                                                }
+                                            })
                                     } else {
                                         const newQuantity = queryItemQuantity - answer.productChoiceQuantity;
                                         const totalOrderCost = queryItemCost * queryItemQuantity;
-                                        console.log(`Checking/testing new quantity value: ${newQuantity}`);
+                                        console.log(`\nChecking/testing new quantity value: ${newQuantity}`);
                                         connection.query(
                                             `UPDATE products SET stock_quantity = ${newQuantity} WHERE id = ${idOfProductChoice}`,
                                             function (err, res) {
